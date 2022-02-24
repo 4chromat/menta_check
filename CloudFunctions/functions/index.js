4a11c919ab22e36799855960dde05b4e9f460d87
Cynthia <cynth.alexander@gmail.com>
2022-02-24 11:44:21 -0800

    Just changed spacing with auto format to not confuse w changes later

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -17,58 +17,58 @@ exports.checkWhiteList = functions.https.onRequest(async (request, response) => 
 
     //let { body: { root_domain, match } } = request;
     console.log(root_domain)
-    var mentaSnapshot = null ;
+    var mentaSnapshot = null;
     var database = admin.database();
 
     // first check if domain in allowlist:
     var allowLRef = await database.ref("allowlist").once('value');
-    if(allowLRef.numChildren() > 0) {
-        if(match == "root_domain") {
+    if (allowLRef.numChildren() > 0) {
+        if (match == "root_domain") {
             mentaSnapshot = await database.ref('allowlist').orderByChild('root_domain').equalTo(root_domain).once('value');
         }
-        if(match == "base_slug") {
+        if (match == "base_slug") {
             mentaSnapshot = await database.ref('allowlist').orderByChild('base_slug').equalTo(root_domain).once('value');
         }
-        if(match == "base_twitter") {
+        if (match == "base_twitter") {
             mentaSnapshot = await database.ref('allowlist').orderByChild('base_twitter').equalTo(root_domain).once('value');
         }
-        if(mentaSnapshot!= null && mentaSnapshot.val() != null) {
-            mentaSnapshot.forEach(function(data) {
+        if (mentaSnapshot != null && mentaSnapshot.val() != null) {
+            mentaSnapshot.forEach(function (data) {
                 var id = data.val().id;
                 console.log("Result allow " + id)
                 var result = data.val().result.rating;
-                response.json({result:result});
+                response.json({ result: result });
             });
             return;
         }
     }
     // sencond check if domain in curated:
     var curateRef = await database.ref("curated").once('value');
-    if(curateRef.numChildren() > 0) {
-        if(match == "root_domain") {
+    if (curateRef.numChildren() > 0) {
+        if (match == "root_domain") {
             mentaSnapshot = await database.ref('curated').orderByChild('root_domain').equalTo(root_domain).once('value');
         }
-        if(match == "base_slug") {
+        if (match == "base_slug") {
             mentaSnapshot = await database.ref('curated').orderByChild('base_slug').equalTo(root_domain).once('value');
         }
-        if(match == "base_twitter") {
+        if (match == "base_twitter") {
             mentaSnapshot = await database.ref('curated').orderByChild('base_twitter').equalTo(root_domain).once('value');
         }
-        if(mentaSnapshot!= null && mentaSnapshot.val() != null) {
-            mentaSnapshot.forEach(function(data) {
+        if (mentaSnapshot != null && mentaSnapshot.val() != null) {
+            mentaSnapshot.forEach(function (data) {
                 var id = data.val().id;
                 console.log("Result curated " + id)
                 var result = data.val().result.rating;
-                response.json({result:result});
+                response.json({ result: result });
             });
         }
     }
-   console.log("Result nothing " )
-  
+    console.log("Result nothing ")
+
     // Send back a message that we've successfully written the message
     // res.json({result: `Message with ID: ${writeResult.id} added.`});
-  // functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("NOTHING");
+    // functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("NOTHING");
 });
 
 // Cloud function to add metaobj
@@ -79,42 +79,42 @@ exports.addMetaObj = functions.https.onRequest(async (request, response) => {
 
     let time = Date.now()
     var database = admin.database();
-   
+
     try {
         // first check if domain in allowlist:
         var mentaobjRef = await database.ref("mentalog").once('value');
-        const count = mentaobjRef.numChildren()? mentaobjRef.numChildren(): 0;
-        const info =  {
-            id:count,
-            timestamp:time,
-            front_tab:mentaObj.frontTab,
-            root_domain:mentaObj.rootDomain,
-            base_website:mentaObj.baseWebsite,
-            base_slug:mentaObj.baseSlug,
-            base_twitter:mentaObj.baseTwitter,
-            rate:mentaObj.rate,
-            result:mentaObj.result
+        const count = mentaobjRef.numChildren() ? mentaobjRef.numChildren() : 0;
+        const info = {
+            id: count,
+            timestamp: time,
+            front_tab: mentaObj.frontTab,
+            root_domain: mentaObj.rootDomain,
+            base_website: mentaObj.baseWebsite,
+            base_slug: mentaObj.baseSlug,
+            base_twitter: mentaObj.baseTwitter,
+            rate: mentaObj.rate,
+            result: mentaObj.result
         }
 
-        const metaAddRef = database.ref("mentalog/"+count)
+        const metaAddRef = database.ref("mentalog/" + count)
         metaAddRef.update(info);
         console.log("done adding mentalog")
-       
+
         // if rate > B ADD allow list:
-        if(mentaObj.rate == 'B' || mentaObj.rate == "A" || mentaObj.rate == "A+") {
+        if (mentaObj.rate == 'B' || mentaObj.rate == "A" || mentaObj.rate == "A+") {
             var allowRef = await database.ref("allowlist").once('value');
-            const countAllow = allowRef.numChildren()? allowRef.numChildren(): 0;
-            const info =  {
-                id:countAllow,
-                timestamp:time,
-                front_tab:mentaObj.frontTab,
-                root_domain:mentaObj.rootDomain,
-                base_slug:mentaObj.baseSlug,
-                base_twitter:mentaObj.baseTwitter,
-                rate:mentaObj.rate,
-                result:mentaObj.result
+            const countAllow = allowRef.numChildren() ? allowRef.numChildren() : 0;
+            const info = {
+                id: countAllow,
+                timestamp: time,
+                front_tab: mentaObj.frontTab,
+                root_domain: mentaObj.rootDomain,
+                base_slug: mentaObj.baseSlug,
+                base_twitter: mentaObj.baseTwitter,
+                rate: mentaObj.rate,
+                result: mentaObj.result
             }
-            const allowAddRef = database.ref("allowlist/"+countAllow)
+            const allowAddRef = database.ref("allowlist/" + countAllow)
             allowAddRef.update(info);
             console.log("done adding allowlist")
         }
@@ -132,22 +132,22 @@ exports.addEventLog = functions.https.onRequest(async (request, response) => {
 
     let time = Date.now()
     var database = admin.database();
-   
+
     try {
         // first check if domain in allowlist:
         var logRef = await database.ref("eventlog").once('value');
-        const count = logRef.numChildren()? logRef.numChildren(): 0;
-        const info =  {
-            id:count,
-            timestamp:time,
-            front_tab:dbinfo.front_tab,
-            action:dbinfo.action,
-            rate:dbinfo.rate
+        const count = logRef.numChildren() ? logRef.numChildren() : 0;
+        const info = {
+            id: count,
+            timestamp: time,
+            front_tab: dbinfo.front_tab,
+            action: dbinfo.action,
+            rate: dbinfo.rate
         }
-        const logAddRef = database.ref("eventlog/"+count)
+        const logAddRef = database.ref("eventlog/" + count)
         logAddRef.update(info);
         console.log("done adding eventlog")
-    
+
         response.send("Done!");
     }
     catch {
